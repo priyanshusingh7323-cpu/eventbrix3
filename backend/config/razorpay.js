d03a70a7439be54d2c09d25dee2f0034bbcab572
priyanshusingh7323-cpu <priyanshusingh7323@gmail.com>
2025-11-25 19:38:52 +0530

    Delete backend directory

--- backend/config/razorpay.js
+++
@@ -1,8 +0,0 @@
-const Razorpay = require("razorpay");
-
-const razorpay = new Razorpay({
-  key_id: "rzp_test_Rjy29zTH71OIkZ",
-  key_secret: "jSMZLXdOax7nxW4r0a6T1Dcl"
-});
-
-module.exports = razorpay;
