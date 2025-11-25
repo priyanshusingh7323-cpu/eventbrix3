c0bea5c6f97505f3733186dbb80dd47379b7500c
priyanshusingh7323-cpu <priyanshusingh7323@gmail.com>
2025-11-25 19:31:01 +0530

    Add files via upload

---
+++ backend/config/razorpay.js
@@ -0,0 +1,8 @@
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+  key_id: "rzp_test_Rjy29zTH71OIkZ",
+  key_secret: "jSMZLXdOax7nxW4r0a6T1Dcl"
+});
+
+module.exports = razorpay;
