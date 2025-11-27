a5f16f47839db0f46d036a9e3c7078e0006abf66
priyanshusingh7323-cpu <priyanshusingh7323@gmail.com>
2025-11-27 16:45:06 +0530

    Add files via upload

--- eventbrix3/config/razorpay.js
+++ eventbrix3/config/razorpay.js
@@ -2,16 +2,13 @@ const Razorpay = require("razorpay");
 
 let razorpay;
 
-// ================================
-//  RAZORPAY HARDCODED (Option A)
-// ================================
 try {
   razorpay = new Razorpay({
-    key_id: "rzp_test_Rjy29zTH71OIkZ",        // SAME KEY
-    key_secret: "jSMZLXdOax7nxW4r0a6T1Dcl",   // SAME SECRET
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
   });
 
-  console.log("✔ Razorpay initialized successfully");
+  console.log("✔ Razorpay initialized with ENV keys");
 } catch (err) {
   console.error("❌ Razorpay Initialization Error:", err.message);
 }
