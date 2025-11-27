a7eeacd85e32a8c05cd04a6f0e9c1b20b755ce1b
priyanshusingh7323-cpu <priyanshusingh7323@gmail.com>
2025-11-27 15:57:15 +0530

    Add files via upload

--- eventbrix3/config/firebaseAdmin.js
+++ eventbrix3/config/firebaseAdmin.js
@@ -1,16 +1,13 @@
 const admin = require("firebase-admin");
 const path = require("path");
 
-// Secret file ka correct absolute path on Render
-const serviceAccountPath = path.resolve("/etc/secrets/service-account-key.json");
+// ALWAYS load from Render secret files folder
+const serviceAccountPath = "/etc/secrets/service-account-key.json";
 
-// Load JSON key
 const serviceAccount = require(serviceAccountPath);
 
-// Initialize Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// Export admin instance
 module.exports = admin;
